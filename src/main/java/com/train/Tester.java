3bc3d973f4a2f17a4ff4f31df1bdfef2aea20760
3A532088 <paileo007007@gmail.com>
2019-04-30 09:53:37 +0800

    homework2

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -5,12 +5,18 @@ import java.util.Scanner;
 public class Tester {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        System.out.println("Please enter number of tickets: ");
-        int  totaltikets=scanner.nextInt();
-        System.out.println("How many round-trip tickets: ");
-        int roundtrip=scanner.nextInt();
 
-        Ticket ticket=new Ticket(totaltikets,roundtrip);
-        ticket.print();
+        while(true){
+            System.out.println("Please enter number of tickets: ");
+            int totaltikets=scanner.nextInt();
+            if(totaltikets == -1){
+                break;
+            }
+            System.out.println("How many round-trip tickets: ");
+            int roundtrip=scanner.nextInt();
+            Ticket ticket=new Ticket(totaltikets,roundtrip);
+            ticket.print();
+        }
+
     }
 }
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -5,26 +5,27 @@ import java.util.*
 fun main(args: Array<String>) {
     val scanner= Scanner(System.`in`)
 
-    print("Please enter number of tickets: ")
-    var totaltrikets=scanner.nextInt()
-
-    print("How many round-trip tickets: ")
-    var roundtrip=scanner.nextInt()
-
-    var tricketKotlin=TricketKotlin(totaltrikets,roundtrip)
-    tricketKotlin.print()
+    while(true){
+        print("Please enter number of tickets: ")
+        var totaltrikets=scanner.nextInt()
+        if(totaltrikets==-1){
+            break
+        }
+        print("How many round-trip tickets: ")
+        var roundtrip=scanner.nextInt()
+        var tricketKotlin=TricketKotlin(totaltrikets,roundtrip)
+        tricketKotlin.print()
+    }
 }
 
 
 class TricketKotlin(var totaltikets:Int,var roundtrip:Int){
-    var onewaytiketsprice=1000;
-    var roundtipprice=2000;
-    var discount=9;
+    var onewaytiketsprice=1000
+    var roundtipprice=2000
+    var discount=9
     var total=(totaltikets-roundtrip)*onewaytiketsprice+roundtrip*roundtipprice*discount/10
 
     fun print(){
-        println("Total tickets:"+totaltikets);
-        println("Round-trip:"+roundtrip);
-        println("Total:"+total);
+        println("Total tickets:$totaltikets\nRound-trip:$roundtrip\nTotal:$total")
     }
 }
